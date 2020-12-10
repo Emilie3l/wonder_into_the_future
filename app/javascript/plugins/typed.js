8b90985a771e9e521d308c91ed0777b418790b6b
Emilie Lockey <emilie.lockey@gmail.com>
2020-12-10 18:10:33 +0000

    Fixed banner and dynamic typing

--- app/javascript/plugins/typed.js
+++ app/javascript/plugins/typed.js
@@ -1,1 +1,16 @@
-typed.js
+import Typed from 'typed.js';
+
+const initTyped = () => {
+    var options = {
+    strings: ['to the future.'],
+    typeSpeed: 80,
+    loop: true,
+    loopCount: Infinity,
+    startDelay: 50,
+    backSpeed: 80,
+  };
+
+  var typed = new Typed('#typed', options);
+}
+
+export { initTyped };
