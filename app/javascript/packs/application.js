7e9d31dda50b478a703011e3acf417405126ef99
Emilie Lockey <emilie.lockey@gmail.com>
2021-02-16 09:34:30 +0000

    Deleted mapbox

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,7 +25,6 @@ require("channels")
 // External imports
 import "bootstrap";
 import { initTyped } from "../plugins/typed"
-import { initMapbox } from '../plugins/init_mapbox';
 
 
 // Internal imports, e.g:
@@ -35,5 +34,4 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initTyped();
-  initMapbox();
 });
